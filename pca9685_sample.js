b0f7b9c671f868ce21517645c2a7c5495d92c9d5
SoRA_X7 <x7@warp-d.com>
2024-10-28 18:40:02 +0900

    Update servo control code to include 0 degree position and adjust PWM pulse range to +-100%

--- pca9685_sample.js
+++ pca9685_sample.js
@@ -1,5 +1,9 @@
 import { requestI2CAccess } from "./node_modules/node-web-i2c/index.js";
 import PCA9685 from "@chirimen/pca9685";
+
+const min = 0.9e-3;
+const max = 2.1e-3;
+
 const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
 
 main();
@@ -8,18 +12,18 @@ async function main() {
   const i2cAccess = await requestI2CAccess();
   const port = i2cAccess.ports.get(1);
   const pca9685 = new PCA9685(port, 0x40);
-  // servo setting for sg90
-  // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
-  await pca9685.init(0.001, 0.002, 30);
+  // servo setting for ft90r
+  // Servo PWM pulse: min=0.0009[sec], max=0.0021[sec] angle=+-100[%]
+  await pca9685.init(min, max, 100);
   for (;;) {
     await pca9685.setServo(0, -5);
-    console.log("-5 deg");
+    console.log("-5%");
     await sleep(1000);
-    await pca9685.setServo(0, 20);
-    console.log("20 deg");
+    await pca9685.setServo(0, 100);
+    console.log("100%");
     await sleep(1000);
     await pca9685.setServo(0, 0);
-    console.log("0 deg");
+    console.log("0%");
     await sleep(1000);
   }
 }
